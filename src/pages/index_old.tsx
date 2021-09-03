6755921aa0b8e95b919a4eaf918464a916984530
Gabriel G <gabriel.giarratana@gmail.com>
2021-09-02 19:54:18 -0500

    Pruning

---
+++ src/pages/index_old.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/ui/Layout';
+import Intro from 'components/modules/Intro';
+import Contact from 'components/modules/Contact';
+
+const HomePage = () => (
+  <>
+    <Layout>
+      <Intro />
+      <Contact />
+    </Layout>
+  </>
+);
+
+export default HomePage;
