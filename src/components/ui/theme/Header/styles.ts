63e9fbd4b03a88367768747381783dcf952cc9b1
Gabriel G <gabriel.giarratana@gmail.com>
2021-09-04 12:22:33 -0500

    Fixed header and overlay transition

--- src/components/ui/theme/Header/styles.ts
+++ src/components/ui/theme/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   background: ${({ theme }) => (theme === 'light' ? '#fff' : '#22262a')};
   width: 100%;
-  position: fixed;
+  position: sticky;
   z-index: 1;
   box-shadow: rgba(0, 0, 0, 0.6) 2px 0px 20px 0px;
 `;
@@ -17,13 +17,13 @@ export const Overlay = styled.div<OverlayProps>`
   background: rgba(0, 0, 0, 0.7);
   width: 100%;
   height: 100%;
-  display: none;
+  opacity: 0;
   transition: 0.4s;
 
   ${({ sidebar }) =>
     sidebar &&
     `
-      display: block;
+      opacity: 100;
       z-index: 4;  
   `}
 `;
