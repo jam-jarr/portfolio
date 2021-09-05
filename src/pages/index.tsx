41bb4318aaffff42d754a4e4972170639f75b909
Gabriel G <gabriel.giarratana@gmail.com>
2021-09-05 02:57:27 -0500

    Move header hierarchy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Intro from "components/modules/Intro";
 import Layout from "components/ui/Layout";
 import Footer from "components/ui/theme/Footer";
+import Header from "components/ui/theme/Header";
 
 const HomePage = () => (
-  <>
-    <Layout>
-      <Intro />
-      <Footer />
-    </Layout>
-  </>
+  <Layout>
+    <Header />
+    <Intro />
+    <Footer />
+  </Layout>
 )
 
 export default HomePage;
